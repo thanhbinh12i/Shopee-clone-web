5d2dac2dd120f79dc7efb3208ec6846ec3489d4e
thanhbinh12ii <nptbinh17092004@gmail.com>
2024-11-15 23:52:20 +0700

    ui header

--- src/useRouteElements.tsx
+++ src/useRouteElements.tsx
@@ -2,9 +2,17 @@ import { useRoutes } from "react-router-dom"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import RegisterLayout from "./layouts/RegisterLayout"
+import MainLayout from "./layouts/MainLayout"
 
 export default function useRouteElements() {
   const routeElements = useRoutes([
+    {
+      path: '/',
+      element: (
+        <MainLayout>
+        </MainLayout>
+      )
+    },
     {
       path: '/login',
       element: (
